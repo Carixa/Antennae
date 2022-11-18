816eff734bb0a0e04a0dc3ead7fea1d730214fed
Carixa <nadiamarsal18@gmail.com>
2022-11-18 19:02:14 -0300

    Actualización 2

---
+++ assets/data.js
@@ -0,0 +1,156 @@
+const productsData = [
+  {
+    id: 1,
+    name: 'Andromeda',
+    bid: 299.99,
+    user: 'HarryStiless',
+    category: 'boreales',
+    userImg: './assets/img/user.png',
+    cardImg: './assets/img/products/andro-boreal.png',
+  },
+  {
+    id: 2,
+    name: 'Aries',
+    bid: 547.79,
+    user: 'FireFox',
+    category: 'boreales',
+    userImg: './assets/img/user.png',
+    cardImg: './assets/img/products/aries-boreal.png',
+  },
+  {
+    id: 3,
+    name: 'Pisces',
+    bid: 449.29,
+    user: 'JulieTa',
+    category: 'boreales',
+    userImg: './assets/img/user.png',
+    cardImg: './assets/img/products/pisces-boreal.png',
+  },
+  {
+    id: 4,
+    name: 'Cancer',
+    bid: 254.35,
+    user: 'Duki',
+    category: 'primavera',
+    userImg: './assets/img/user.png',
+    cardImg: './assets/img/products/cancer-primavera.jpg',
+  },
+  {
+    id: 5,
+    name: 'Leo',
+    bid: 414.52,
+    user: 'MaylenD',
+    category: 'primavera',
+    userImg: './assets/img/user.png',
+    cardImg: './assets/img/products/leo-primavera.jpg',
+  },
+  {
+    id: 6,
+    name: 'Virgo',
+    bid: 721.44,
+    user: 'WalterMa',
+    category: 'primavera',
+    userImg: './assets/img/user.png',
+    cardImg: './assets/img/products/virgo-primavera.jpg',
+  },
+  {
+    id: 7,
+    name: 'Draco',
+    bid: 468.20,
+    user: 'ElianaF',
+    category: 'verano',
+    userImg: './assets/img/user.png',
+    cardImg: './assets/img/products/draco-verano.jpg',
+  },
+  {
+    id: 8,
+    name: 'Libra',
+    bid: 128.15,
+    user: 'ReallyIDK',
+    category: 'verano',
+    userImg: './assets/img/user.png',
+    cardImg: './assets/img/products/libra-verano.jpg',
+  },
+  {
+    id: 9,
+    name: 'Scorpius',
+    bid: 200.5,
+    user: 'BadBoy',
+    category: 'verano',
+    userImg: './assets/img/user.png',
+    cardImg: './assets/img/products/scorpius-verano.jpg',
+  },
+  {
+    id: 10,
+    name: 'Tucana',
+    bid: 418.35,
+    user: 'YankeeDad',
+    category: 'otoño',
+    userImg: './assets/img/user.png',
+    cardImg: './assets/img/products/tucana-otoño.jpg',
+  },
+  {
+    id: 11,
+    name: 'Aquarius',
+    bid: 382.75,
+    user: 'DracoMalf',
+    category: 'otoño',
+    userImg: './assets/img/user.png',
+    cardImg: './assets/img/products/aquarius-otoño.jpg',
+  },
+  {
+    id: 12,
+    name: 'Octans',
+    bid: 264.51,
+    user: 'Bennito',
+    category: 'otoño',
+    userImg: './assets/img/user.png',
+    cardImg: './assets/img/products/octans-otoño.jpg',
+  },
+  {
+    id: 13,
+    name: 'Gemini',
+    bid: 815.27,
+    user: 'Carixa',
+    category: 'invierno',
+    userImg: './assets/img/user.png',
+    cardImg: './assets/img/products/gemini-invierno.jpg',
+  },
+  {
+    id: 14,
+    name: 'Orion',
+    bid: 500.55,
+    user: 'StarsWarsLove',
+    category: 'invierno',
+    userImg: './assets/img/user.png',
+    cardImg: './assets/img/products/orion-invierno.jpg',
+  },
+  {
+    id: 15,
+    name: 'Puppis',
+    bid: 168.46,
+    user: 'HoldPrus',
+    category: 'invierno',
+    userImg: './assets/img/user.png',
+    cardImg: './assets/img/products/puppis-invierno.jpg',
+  },
+];
+
+// Para hacer la paginacion de Ver Más
+// Usamos el array de productos y lo dividimos en arrays de size elementos
+const splitProducts = size => {
+  let dividedProducts = [];
+  for (let i = 0; i < productsData.length; i += size) {
+    dividedProducts.push(productsData.slice(i, i + size));
+  }
+  return dividedProducts;
+};
+
+// Funcion para dividir los productos en arrays de 6 y manejar la paginacion
+const productsController = {
+  dividedProducts: splitProducts(2),
+  nextProductsIndex: 1,
+  productsLimit: splitProducts(15).length,
+};
+
+// console.log(productsController);
